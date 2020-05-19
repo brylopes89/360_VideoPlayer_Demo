7d37092f859f8aa438be1365ea639d9931dae5ca
Bryan Lopes <brylopes89@gmail.com>
2020-05-18 22:34:04 -0400

    video player and menu system added

---
+++ Assets/Resources/Scripts/InputManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.iOS;
+using UnityEngine.XR;
+
+public class InputManager : MonoBehaviour
+{   
+    public VideoManager videoManager = null;
+    public List<XRController> controllers = null;
+    public GameObject videoMenu = null;
+    public GameObject player = null;
+    public float distance = 10f;
+
+    private Animator animator = null;    
+
+    private void Start()
+    {
+        animator = videoMenu.GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!videoManager.IsVideoReady)
+            return;
+
+        VRInput();
+        KeyboardInput();
+    }
+    //private void OculusInput()
+    //{
+    //    if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.All))
+    //    {
+    //        videoManager.PauseToggle();
+    //    }
+
+    //    if(OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+    //    {
+
+    //    }
+
+    //    if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+    //    {
+
+    //    }
+
+    //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+    //    {
+
+    //    }
+
+    //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+    //    {
+
+    //    }
+    //}    
+
+    private void VRInput()
+    {
+        foreach (XRController controller in controllers)
+        {
+            if (controller.enableInputActions)
+            {
+                if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool value))
+                {                                       
+                    videoMenu.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
+                    videoMenu.transform.rotation = Quaternion.LookRotation(videoMenu.transform.position - player.transform.position);
+                    OpenMenu();                                       
+                }                    
+            }                
+        }
+    }
+    private void KeyboardInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            videoManager.Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            videoManager.PreviousVideo();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            videoManager.NextVideo();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            videoManager.SeekBack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            videoManager.SeekForward();
+        }
+    }
+
+    private void OpenMenu()
+    {
+        bool isOpen = animator.GetBool("OpenMenu");
+
+        animator.SetBool("OpenMenu", !isOpen);
+    }
+}
---
+++ Assets/Resources/Scripts/VideoManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class VideoEvent : UnityEvent<bool> { }
+public class VideoManager : MonoBehaviour
+{
+    public List<VideoClip> videos = null;
+
+    public VideoEvent onPause = new VideoEvent();
+    public VideoEvent onLoad = new VideoEvent();
+
+    private bool isPaused = false;
+    public bool IsPaused { get { return isPaused; } private set { isPaused = value; onPause.Invoke(isPaused); } }
+
+    private bool isVideoReady = false;
+    public bool IsVideoReady { get { return isVideoReady; } private set { isVideoReady = value; onLoad.Invoke(isVideoReady); } }
+
+    private int index = 0;
+    private VideoPlayer videoPlayer = null;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.seekCompleted += OnComplete;
+        videoPlayer.prepareCompleted += OnComplete;
+        videoPlayer.loopPointReached += OnLoop;
+    }
+
+    private void Start()
+    {
+        StartPrepare(index);   
+    }
+
+    public void Pause()
+    {
+        IsPaused = true;
+        videoPlayer.Pause();       
+    }
+
+    public void Play()
+    {
+        IsPaused = false;        
+        videoPlayer.Play();
+    }
+
+    private void OnDestroy()
+    {
+        videoPlayer.seekCompleted -= OnComplete;
+        videoPlayer.prepareCompleted -= OnComplete;
+        videoPlayer.loopPointReached -= OnLoop;
+    }
+
+    public void SeekForward()
+    {
+        StartSeek(5.0f);
+    }
+
+    public void SeekBack()
+    {
+        StartSeek(-5.0f);
+    }
+
+    private void StartSeek(float seekAmount)
+    {
+        IsVideoReady = false;
+        videoPlayer.time += seekAmount;
+    }
+
+    public void NextVideo()
+    {
+        index++;
+
+        if (index == videos.Count)
+            index = 0;
+
+        StartPrepare(index);
+    }
+
+    public void PreviousVideo()
+    {
+        index--;
+        if (index == -1)
+            index = videos.Count - 1;
+
+        StartPrepare(index);
+    }
+
+    private void StartPrepare(int clipIndex)
+    {
+        IsVideoReady = false;
+        videoPlayer.clip = videos[clipIndex];
+        videoPlayer.Prepare();
+    }
+
+    private void OnComplete(VideoPlayer videoPlayer)
+    {
+        IsVideoReady = true;
+        videoPlayer.Play();
+    }
+
+    private void OnLoop(VideoPlayer videoPlayer)
+    {
+        NextVideo();
+    }   
+}
